781484f5580c3ccd7dc5a299378564abcde3b6cd
Tanjiro322 <tanushtyagi.190912@gmail.com>
2025-09-30 12:48:50 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,49 @@
+// Service Worker for PWA
+const CACHE_NAME = 'clubhouse-v1';
+const urlsToCache = [
+  '/',
+  '/name.html',
+  '/details.html',
+  '/main.html',
+  '/manifest.json'
+];
+
+// Install service worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch from cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Activate and clean old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
